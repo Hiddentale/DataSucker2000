dca41f1968b99cdf69f1d041b2c7d55dddce959e
Johan Voskamp <55276630+Hiddentale@users.noreply.github.com>
2025-02-20 15:05:08 +0100

    Added list decoding functionality

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use serde_json;
-//use std::env;
-use std::fmt;
+pub use serde_json;
+pub use std::env;
+pub use std::fmt;
+pub use std::error::Error;
 
-enum BencodeError {
+pub enum BencodeError {
     EmptyInput,
     MissingColon,
     InvalidLength,
@@ -20,7 +21,7 @@ impl fmt::Display for BencodeError {
     }
 }
 
-fn decode_string(encoded_value: &str) -> Result<serde_json::Value, BencodeError> {
+pub fn decode_string(encoded_value: &str) -> Result<serde_json::Value, BencodeError> {
     let colon_index = encoded_value.find(':').ok_or(BencodeError::MissingColon)?;
     let number = encoded_value[..colon_index].parse::<i64>().map_err(|_| BencodeError::InvalidLength)?;
     let end_index = colon_index + 1 + number as usize;
@@ -31,7 +32,7 @@ fn decode_string(encoded_value: &str) -> Result<serde_json::Value, BencodeError>
     return Ok(serde_json::Value::String(string.to_string()));
 }
 
-fn decode_integer(encoded_value: &str) -> Result<serde_json::Value, BencodeError> {
+pub fn decode_integer(encoded_value: &str) -> Result<serde_json::Value, BencodeError> {
     if encoded_value.chars().last() != Some('e') {
         return Err(BencodeError::InvalidFormat);
     }
@@ -40,9 +41,7 @@ fn decode_integer(encoded_value: &str) -> Result<serde_json::Value, BencodeError
     return Ok(serde_json::Value::Number(integer.into()));
 }
 
-fn decode_list(encoded_value: &str) -> Result<serde_json::Value, BencodeError> {
-    //What to do if nested list? Recurse?
-    println!("Found an l!");
+pub fn decode_list(encoded_value: &str) -> Result<(serde_json::Value, usize), BencodeError> {
     let mut list_data = Vec::new();
     let mut i = 1;
     while i < encoded_value.len() - 1 {
@@ -55,40 +54,44 @@ fn decode_list(encoded_value: &str) -> Result<serde_json::Value, BencodeError> {
                 let string_end = true_colon_index + 1 + length;
                 list_data.push(decode_string(&encoded_value[i..string_end])?);
                 i = string_end;
-                println!("{}", &encoded_value[i..]);
-                println!("{:?}", list_data);
             },
             'i' => {
                 let mut integer_end = i;
-                while encoded_value[integer_end] != 'e' {
+                while encoded_value.chars().nth(integer_end).ok_or(BencodeError::EmptyInput)? != 'e' {
                     integer_end += 1
                 }
+                integer_end +=1;
                 list_data.push(decode_integer(&encoded_value[i..integer_end])?);
                 i = integer_end;
-                println!("{}", &encoded_value[i..]);
-                println!("{:?}", list_data);
             },
+            'l' => {
+                let (nested_list, new_index) = decode_list(&encoded_value[i..])?;
+                list_data.push(nested_list);
+                i += new_index;
+            },
+            'e' => break,
+
             invalid_character => {
                 eprintln!("Invalid initial character: {}", invalid_character);
                 return Err(BencodeError::InvalidFormat);
             }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
         }
     }
-    return Ok(serde_json::Value::Array(list_data));
-    //Whenever we find an integer => call decode_integer, whenever we find a string => call decode_string
-    //So max O(n) time complexity.
+    return Ok((serde_json::Value::Array(list_data), i + 1));
+
 }
-fn decode_dictionary(_encoded_value: &str) -> Result<serde_json::Value, BencodeError> {
+
+pub fn decode_dictionary(_encoded_value: &str) -> Result<serde_json::Value, BencodeError> {
     unimplemented!("Dictionary decoding has not been implemented yet!");
 }
 
-fn decode_bencoded_value(encoded_value: &str) -> Result<serde_json::Value, BencodeError> {
+pub fn decode_bencoded_value(encoded_value: &str) -> Result<serde_json::Value, BencodeError> {
     match encoded_value.chars().next() {
         None => return Err(BencodeError::EmptyInput),
         Some(first_character) => match first_character {
             '0'..='9' => decode_string(encoded_value),
             'i' => decode_integer(encoded_value),
-            'l' => decode_list(encoded_value),
+            'l' => decode_list(encoded_value).map(|(value, _)| value),
             'd' => decode_dictionary(encoded_value),
             invalid_character => {
                 eprintln!("Invalid initial character: {}", invalid_character);
@@ -99,31 +102,24 @@ fn decode_bencoded_value(encoded_value: &str) -> Result<serde_json::Value, Benco
 }
 
 fn main() {
-    let empty_input = "";
-    let decoded_empty_value = decode_bencoded_value(empty_input);
-    match decoded_empty_value {
-        Ok(value) => println!("{}", value),
-        Err(error) => println!("Error: {}", error) 
-    }
-    let test_argument = "l5:helloi52ee";
-    let decoded_value = decode_bencoded_value(test_argument);
-    match decoded_value {
-        Ok(value) => println!("{}", value),
-        Err(error) => println!("Error: {}", error) 
-    }
-
-    //let args: Vec<String> = env::args().collect();
-    //let command = &args[1];
-
-    //if command == "decode" {
-        //let encoded_value = &args[2];
-        //let decoded_value = decode_bencoded_value(encoded_value);
-        //match decoded_value {
-            //Ok(value) => println!("{}", value.to_string()),
-            //Err(error) => eprintln!("Error: {}", error)
-        //}
-        
-    //} else {
-        //println!("unknown command: {}", args[1])
+    //let encoded_value = "lli4ei5eee";
+    //let decoded_value = decode_bencoded_value(encoded_value);
+    //match decoded_value {
+        //Ok(value) => println!("{}", value.to_string()),
+        //Err(error) => eprintln!("Error: {}", error)
     //}
+    let args: Vec<String> = env::args().collect();
+    let command = &args[1];
+
+    if command == "decode" {
+        let encoded_value = &args[2];
+        let decoded_value = decode_bencoded_value(encoded_value);
+        match decoded_value {
+            Ok(value) => println!("{}", value.to_string()),
+            Err(error) => eprintln!("Error: {}", error)
+        }
+        
+    } else {
+        println!("unknown command: {}", args[1])
+    }
 }
